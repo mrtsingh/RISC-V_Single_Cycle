485c6858ded1c8a77c023425908fc4b607519b0b
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-13 22:59:00 +0530

    Update ALU_Decoder.v

--- ALU_Decoder.v
+++ ALU_Decoder.v
@@ -1,34 +1,24 @@
 `timescale 1ns /1ps
 
 
-module Main_Decoder(Op,
-                    RegWrite,
-                    ImmSrc,
-                    ALUSrc,
-                    MemWrite,
-                    ResultSrc,
-                    Branch,
-                    ALUOp);
-                    
-    input [6:0]Op;
-    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
-    output [1:0]ImmSrc,ALUOp;
+module ALU_Decoder(ALUOp,
+                   funct3,
+                   funct7,
+                   op,
+                   ALUControl);
 
-    assign RegWrite = (Op == 7'b0000011 | Op == 7'b0110011) ? 1'b1 :
-                                                              1'b0 ;
-    assign ImmSrc = (Op == 7'b0100011) ? 2'b01 : 
-                    (Op == 7'b1100011) ? 2'b10 :    
-                                         2'b00 ;
-    assign ALUSrc = (Op == 7'b0000011 | Op == 7'b0100011) ? 1'b1 :
-                                                            1'b0 ;
-    assign MemWrite = (Op == 7'b0100011) ? 1'b1 :
-                                           1'b0 ;
-    assign ResultSrc = (Op == 7'b0000011) ? 1'b1 :
-                                            1'b0 ;
-    assign Branch = (Op == 7'b1100011) ? 1'b1 :
-                                         1'b0 ;
-    assign ALUOp = (Op == 7'b0110011) ? 2'b10 :
-                   (Op == 7'b1100011) ? 2'b01 :
-                                        2'b00 ;
+    input [1:0]ALUOp;
+    input [2:0]funct3;
+    input [6:0]funct7,op;
+    output [2:0]ALUControl;
 
+   
+    assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+                        (ALUOp == 2'b01) ? 3'b001 :
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} == 2'b11)) ? 3'b001 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} != 2'b11)) ? 3'b000 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
+                                                                  3'b000 ;
 endmodule
