93489b8e791b5b9d796a64d957244b874b923987
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-13 22:56:02 +0530

    Update Instruction_Memory.v

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -1,20 +1,22 @@
-module instr_mem(A,rst,RD);
-
-    input[31:0] A; //input address to the instruction memory
-    input rst;
-
-    output[31:0] RD;
-
-    //Created a memory here
-    reg[31:0]mem[1023:0]; //1024 registers with each registers of size 32 bits
+`timescale 1ns /1ps
 
 
-    assign RD = (rst == 1'b0) ? 32'h00000000 : mem[A[31:2]];
+module Instruction_Memory(rst,
+                          A,
+                          RD);
 
-    initial
-    begin
-        mem[0] = 32'hFFC4A303;
-    end
+  input rst;
+  input [31:0]A;
+  output [31:0]RD;
 
+  reg [31:0] mem [1023:0];
+  
+  assign RD = (~rst) ? {32{1'b0}} : mem[A[31:2]];
+
+  initial begin
+    mem[0] = 32'h0062E233;
+    // mem[1] = 32'h00B62423;
+
+  end
 
 endmodule
