58e33b6e80ff508515980482cad776fb352e3ea3
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-13 23:00:30 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,55 +1,36 @@
 `timescale 1ns /1ps
-module alu(A,B,ALUcontrol,Result,Z_F,N_F,V_F,C_F);
-    //declaring inputs
-    input [31:0] A,B;
-    input [2:0] ALUcontrol;
 
-    //declaring outputs
-    output[31:0] Result;
-    output Z_F,N_F,V_F,C_F; //Single Bit Flags(Zero flag, Negative Flag,Overflow Flag, Carry Flag)
 
-    //declaring wires
-    wire[31:0] a_and_b;
-    wire[31:0] a_or_b;
-    wire[31:0] not_b;
-    wire[31:0] mux_1;
-    wire[31:0] sum;
-    wire[31:0] mux_2;
-    wire[31:0] slt;
+module ALU(A,
+           B,
+           Result,
+           ALUControl,
+           OverFlow,
+           Carry,
+           Zero,
+           Negative);
 
-    wire cout;//To store carry information
+    input [31:0]A,B;
+    input [2:0]ALUControl;
+    output Carry,OverFlow,Zero,Negative;
+    output [31:0]Result;
 
-    assign a_and_b = A & B;
-    assign a_or_b = A | B;
+    wire Cout;
+    wire [31:0]Sum;
 
-    //not operation on b
-    assign not_b = ~B;
+    assign {Cout,Sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? Sum :
+                    (ALUControl == 3'b001) ? Sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(Sum[31])} : {32{1'b0}};
     
-    //Ternary operator for generating mux
-    assign mux_1 = (ALUcontrol[0] == 1'b0) ? B : not_b;
+    assign OverFlow = ((Sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
 
-    //Addition Subtraction
-
-    assign {cout,sum} = A + mux_1 + ALUcontrol[0];
-
-    //zero Extension for set less than
-
-    assign slt = {31'b0000000000000000000000000000000,sum[30]}; //n-1 th bit of sum
-    //designing 4 cross 1 mux
-    assign mux_2 =  (ALUcontrol[2:0] == 2'b000) ? sum : 
-                    (ALUcontrol[2:0] == 2'b001) ? sum :
-                    (ALUcontrol[2:0] == 2'b010) ? a_and_b :
-                    (ALUcontrol[2:0] == 2'b011) ? a_or_b :
-                    (ALUcontrol[2:0] == 2'b101) ? slt : 32'h00000000;
-
-
-
-    assign Result = mux_2;
-
-    //Flags Assignment
-
-    assign Z_F = &(~Result); //Zero Flag
-    assign N_F = Result[31]; // Negative Flag
-    assign C_F = cout & (~ALUcontrol[1]); // Carry Flag
-    assign V_F = (ALUcontrol[1]) & (A[31] ^ sum[31]) & (~(A[31] ^ B[31] ^ ALUcontrol[0])); //Overflow Flag
 endmodule
