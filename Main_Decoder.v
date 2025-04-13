552c8e028611d6d6cf68ed25b78dabb093ba014d
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-13 22:59:42 +0530

    Update Main_Decoder.v

--- Main_Decoder.v
+++ Main_Decoder.v
@@ -1,28 +1,34 @@
-//Making this as Combinational Circuit so that no concept of delay
-module main_dec(op,zero,Regwri,Memwri,ResultSrc,ALUSrc,ImmSrc,ALUop,PCSrc);
+`timescale 1ns /1ps
 
-    //Inputs, outputs
-    input zero;
-    input [6:0] op;
-    output Regwri,Memwri,ResultSrc,ALUSrc,PCSrc;
-    output [1:0] ImmSrc,ALUop;
+//Made as combinational circuit so that no concept of delay occurs
+module Main_Decoder(Op,
+                    RegWrite,
+                    ImmSrc,
+                    ALUSrc,
+                    MemWrite,
+                    ResultSrc,
+                    Branch,
+                    ALUOp);
+                    
+    input [6:0]Op;
+    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
+    output [1:0]ImmSrc,ALUOp;
 
-    wire branch;
+    assign RegWrite = (Op == 7'b0000011 | Op == 7'b0110011) ? 1'b1 :
+                                                              1'b0 ;
+    assign ImmSrc = (Op == 7'b0100011) ? 2'b01 : 
+                    (Op == 7'b1100011) ? 2'b10 :    
+                                         2'b00 ;
+    assign ALUSrc = (Op == 7'b0000011 | Op == 7'b0100011) ? 1'b1 :
+                                                            1'b0 ;
+    assign MemWrite = (Op == 7'b0100011) ? 1'b1 :
+                                           1'b0 ;
+    assign ResultSrc = (Op == 7'b0000011) ? 1'b1 :
+                                            1'b0 ;
+    assign Branch = (Op == 7'b1100011) ? 1'b1 :
+                                         1'b0 ;
+    assign ALUOp = (Op == 7'b0110011) ? 2'b10 :
+                   (Op == 7'b1100011) ? 2'b01 :
+                                        2'b00 ;
 
-    assign Regwri = ((op == 7'b0000011) | (op == 7'b0110011)) ? 1'b1  : 1'b0;
-    
-    assign Memwri = (op == 7'b0100011) ? 1'b1  : 1'b0;
-
-    assign ResultSrc = (op == 7'b0000011) ? 1'b1 : 1'b0;
-
-    assign ALUSrc = ((op == 7'b0000011) | (op == 7'b0100011)) ? 1'b1: 1'b0;
-
-    assign branch = (op == 7'b1100011)?1'b1:1'b0;
-
-    assign ImmSrc = (op == 7'b0100011) ? 2'b01 : (op == 7'b1100011) ? 2'b10 : 2'b00;
-
-    assign ALUop = (op == 7'b0110011) ? 2'b01 : (op == 7'b1100011) ? 2'b10 : 2'b00;
-
-    assign PCSrc = zero & branch;
-    
 endmodule
