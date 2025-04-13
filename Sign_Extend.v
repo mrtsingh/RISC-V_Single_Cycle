c09ce925b0390b82c4af0914ac23182c768f7ca6
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-13 22:54:39 +0530

    Update Sign_Extend.v

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -1,8 +1,15 @@
-module se(In,Imm_Ext);
-    input[31:0] In;
-    output[31:0] Imm_Ext;
+`timescale 1ns /1ps
 
-    assign Imm_Ext = (In[31]) ? {{20{1'b1}},In[31:20]} : 
-                                {{20{1'b0}},In[31:20]}; //To concatinate according to sign of the number (sign at 12th bit of number)
 
+module Sign_Extend (In,
+                    Imm_Ext,
+                    ImmSrc);
+
+    input [31:0]In;
+    input ImmSrc;
+    output [31:0]Imm_Ext;
+
+    assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
+                                        {{20{In[31]}},In[31:20]};
+                                
 endmodule
