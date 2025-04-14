6ff06384ac3df854338f5f75aaf3902970c97b55
mrtsingh <118091971+mrtsingh@users.noreply.github.com>
2025-04-14 21:38:37 +0530

    Update Sign_Extend.v

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -6,7 +6,7 @@ module Sign_Extend (In,
                     ImmSrc);
 
     input [31:0]In;
-    input ImmSrc;
+  input ImmSrc; //When immediate src is 1 means that the extend block will extend instruction according to S type and when 0 then will extend according to I type
     output [31:0]Imm_Ext;
 
     assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
